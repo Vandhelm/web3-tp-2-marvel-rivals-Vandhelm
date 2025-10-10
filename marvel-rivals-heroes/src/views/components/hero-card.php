db1bec9ccfbecfe3f40c1fd098034dadd1c10709
Vandhelm <vandhelm@gmail.com>
2025-10-10 15:10:13 -0400

    fix: adjust the number of columns in the hero list (grid)

--- marvel-rivals-heroes/src/views/components/hero-card.php
+++ marvel-rivals-heroes/src/views/components/hero-card.php
@@ -1,4 +1,4 @@
-<div class="col">
+<div class="col col-2">
     <h5 class="card-title"><?= htmlspecialchars($hero['name']) ?></h5>
     <p class="card-text"><?= htmlspecialchars($hero['class']) ?></p>
 </div>
