39d415e72da5af27fea0c033a0ef3efbb0bf85c6
Vandhelm <vandhelm@gmail.com>
2025-10-10 13:06:22 -0400

    feat: add safeguard for incorrect filtering (bad user input)

--- marvel-rivals-heroes/src/views/hero-list.php
+++ marvel-rivals-heroes/src/views/hero-list.php
@@ -1,15 +1,21 @@
 <div class="container">
   <div class="row">
 
+    <?php if (empty($filtered)): ?>
+        <p>Aucun héros trouvé.</p>
 
-    <?php foreach ($filtered as $hero): ?>
+    <?php else: ?>
 
-        <div class="col">
-            <h5 class="card-title"><?= htmlspecialchars($hero['name']) ?></h5>
-            <p class="card-text"><?= htmlspecialchars($hero['class']) ?></p>
-        </div>
+        <?php foreach ($filtered as $hero): ?>
 
-    <?php endforeach; ?>
+            <div class="col">
+                <h5 class="card-title"><?= htmlspecialchars($hero['name']) ?></h5>
+                <p class="card-text"><?= htmlspecialchars($hero['class']) ?></p>
+            </div>
+
+        <?php endforeach; ?>
+
+    <?php endif; ?>    
 
 
   </div>
