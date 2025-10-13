b40b084435a5adf09c467604a6532c3cd1c32a48
Vandhelm <vandhelm@gmail.com>
2025-10-13 10:12:08 -0400

    feat: add ability to access details page by clicking on a hero card

--- marvel-rivals-heroes/src/views/components/hero-card.php
+++ marvel-rivals-heroes/src/views/components/hero-card.php
@@ -5,5 +5,7 @@
 -->
 
 <div class="col col-2">
+    <a href="index.php?page=details&hero=<?= format($hero['name'])?>"> 
     <img src="<?= htmlspecialchars($hero['images']['portrait']) ?>" alt="<?= htmlspecialchars($hero['name']) ?>">
+    </a>
 </div>
