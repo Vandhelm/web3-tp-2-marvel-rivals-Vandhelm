a450eefba1022a23d83ff47c9758fa4eda8a30f9
Vandhelm <vandhelm@gmail.com>
2025-10-14 21:56:10 -0400

    feat: add css for searchbar and filter buttons (with responsivity)

--- marvel-rivals-heroes/src/views/components/search-form.php
+++ marvel-rivals-heroes/src/views/components/search-form.php
@@ -1,21 +1,38 @@
-<form method="GET" action="index.php">
+
+<form method="GET" action="index.php" class="d-flex align-items-center bg-transparent py-3">
     
-    <button>Submit</button>
+    <div class="search-bar">
+        <button class="search-button">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
 
-    <input type="text" name="hero_name" id="hero_name" placeholder="Enter hero name"/>
-    <label for="hero_name">Recherche...</label>
+        <input type="text" name="hero_name" id="hero_name" placeholder="Recherche..."/>
 
-    <fieldset>
+    </div>
+    
+    
+    <fieldset class="role-filter">
+        
         <input type="radio" id="all" name="role" value="all"checked/>
-        <label for="all">Tous</label>
+        <label for="all">Tous</label> 
 
         <input type="radio" id="duelist" name="role" value="duelist"/>
-        <label for="duelist">Duelistes</label>
+        <label for="duelist">
+            <img src="public/images/ui/icons/classes/duelist-icon.png" alt="icône de classe">
+            <span>Duelistes</span>
+        </label>
 
         <input type="radio" id="strategist" name="role" value="strategist"/>
-        <label for="strategist">Strategistes</label>
+        <label for="strategist">
+            <img src="public/images/ui/icons/classes/strategist-icon.png" alt="icône de classe">    
+                <span>Strategistes</span>
+        </label>
 
         <input type="radio" id="vanguard" name="role" value="vanguard"/>
-        <label for="vanguard">Vanguards</label>
+        <label for="vanguard">
+            <img src="public/images/ui/icons/classes/vanguard-icon.png" alt="icône de classe">
+            <span>Vanguards</span>
+        </label>
+
     </fieldset>
 </form>
--- marvel-rivals-heroes/src/views/layouts/head.php
+++ marvel-rivals-heroes/src/views/layouts/head.php
@@ -3,6 +3,10 @@
 <link rel="shortcut icon" href="/public/images/favicon.ico" type="image/x-icon">  
 
 <link rel="stylesheet" href="/public/bootstrap/css/bootstrap.css"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
+
+
+
 <link rel="stylesheet" href="/public/stylesheets/styles.css">
 
 <link rel="stylesheet" href="/public/stylesheets/details.css">
